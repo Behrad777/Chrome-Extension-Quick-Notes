690d40acf5fd6306139fc963a0f9622d3a781e2b
Behrad777 <85142047+Behrad777@users.noreply.github.com>
2023-08-08 01:05:00 -0400

    update dynamic create divs

--- popup.js
+++ popup.js
@@ -1,29 +1,56 @@
 // buttons
 
-const savebutton = document.getElementById("saveButton")
-const clearbutton = document.getElementById("clearButton")
+const savebutton = document.getElementById("saveButton");
+const clearbutton = document.getElementById("clearButton");
 
 //elements
 
-const textarea = document.getElementById("textBox")
 
-savebutton.onclick = function() {
-    console.log("you saved: ", textarea.value);
+
+
+//creating dynamic div for saved notes
+let divElement = document.createElement('div');
+
+divElement.classList.add("addnewnote");
+
+
+let containerDiv = document.querySelector('.main');
+
+savebutton.addEventListener("click", displaySavedNotes)
+
+containerDiv.appendChild(divElement);
+
+function displaySavedNotes() {
+    const textarea = document.getElementById("textBox");
+
+    let html = 
+    `<div class = "savednotesarea">
+        <div class="thenotes"> 
+            <div class="note">${textarea.value}</div>
+        </div>
+        <div class = "div-remove-note">
+            <button class="remove-note">X</button>
+        </div>
+    </div>`;
+    if ((textarea.value).trim() !="")
+    divElement.insertAdjacentHTML('afterend', html);
+
     
-
-    chrome.storage.local.set();
-
-
-     
+document.querySelector(".remove-note")
+.addEventListener("click", function(){
+    console.log(this.parentNode);
+    let removeelement = this.parentNode.parentNode;
+    containerDiv.removeChild(removeelement)
+})
 }
 
+
+//clear button function
 
 clearbutton.onclick = function() {
+    const textarea = document.getElementById("textBox");
     console.log("you cleared :", textarea.value);
-    document.getElementById("textBox").value = "";
+    document.getElementById("textBox").value = " ";
 }
 
-
-  
-  
 
