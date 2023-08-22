21876e0e6612a5f5ba1eec5db6d0db3dd482572e
Behrad777 <85142047+Behrad777@users.noreply.github.com>
2023-08-22 00:04:30 -0400

    notes save localstorage

--- popup.js
+++ popup.js
@@ -2,23 +2,45 @@
 
 const savebutton = document.getElementById("saveButton");
 const clearbutton = document.getElementById("clearButton");
-
+const savednoteslist = JSON.parse(localStorage.getItem("notes")) || [];
 //elements
-
 
 
 
 //creating dynamic div for saved notes
 let divElement = document.createElement('div');
-
 divElement.classList.add("addnewnote");
-
-
 let containerDiv = document.querySelector('.main');
+containerDiv.appendChild(divElement);
 
 savebutton.addEventListener("click", displaySavedNotes)
+//load saved notes 
 
-containerDiv.appendChild(divElement);
+function loadSavedNotes(val) {
+    let html = 
+    `<div class = "savednotesarea">
+        <div class="thenotes"> 
+            <div class="note">${val}</div>
+        </div>
+        <div class = "div-remove-note">
+            <button class="remove-note">X</button>
+        </div>
+    </div>`;
+
+    divElement.insertAdjacentHTML('afterend', html);
+    
+    document.querySelector(".remove-note")
+    .addEventListener("click", function(){
+        console.log(this.parentNode);
+        let removeelement = this.parentNode.parentNode;
+        containerDiv.removeChild(removeelement)
+        
+    })
+}
+
+for (let i = 0; i < savednoteslist.length; i++) {
+    loadSavedNotes(savednoteslist[i]);
+}
 
 function displaySavedNotes() {
     const textarea = document.getElementById("textBox");
@@ -35,14 +57,17 @@ function displaySavedNotes() {
     if ((textarea.value).trim() !="")
     divElement.insertAdjacentHTML('afterend', html);
 
+    savednoteslist.push(textarea.value);
+
+    localStorage.setItem("notes",JSON.stringify(savednoteslist));
     
-document.querySelector(".remove-note")
-.addEventListener("click", function(){
-    console.log(this.parentNode);
+document.querySelector(".remove-note").addEventListener("click", function(){
     let removeelement = this.parentNode.parentNode;
     containerDiv.removeChild(removeelement)
 })
 }
+
+
 
 
 //clear button function
@@ -52,5 +77,4 @@ clearbutton.onclick = function() {
     console.log("you cleared :", textarea.value);
     document.getElementById("textBox").value = " ";
 }
-
 
