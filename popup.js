e3af07ab920e0804e8ea8d7760858289e786971c
Behrad777 <behradassa@gmail.com>
2023-11-05 20:44:17 -0500

    Implemented localStorage for saving notes

--- popup.js
+++ popup.js
@@ -1,80 +1,55 @@
-// buttons
-
 const savebutton = document.getElementById("saveButton");
 const clearbutton = document.getElementById("clearButton");
-const savednoteslist = JSON.parse(localStorage.getItem("notes")) || [];
-//elements
+const containerDiv = document.querySelector('.main');
+const textarea = document.getElementById("textBox");
 
+// Load previously saved notes when the extension is opened
+window.addEventListener("DOMContentLoaded", () => {
+    const savedNotes = JSON.parse(localStorage.getItem("notes")) || [];
+    savedNotes.forEach(noteText => {
+        createNoteElement(noteText);
+    });
+});
 
+savebutton.addEventListener("click", () => {
+    const noteText = textarea.value.trim();
+    if (noteText !== "") {
+        createNoteElement(noteText);
 
-//creating dynamic div for saved notes
-let divElement = document.createElement('div');
-divElement.classList.add("addnewnote");
-let containerDiv = document.querySelector('.main');
-containerDiv.appendChild(divElement);
+        // Save the notes to localStorage
+        const savedNotes = JSON.parse(localStorage.getItem("notes")) || [];
+        savedNotes.push(noteText);
+        localStorage.setItem("notes", JSON.stringify(savedNotes));
 
-savebutton.addEventListener("click", displaySavedNotes)
-//load saved notes 
-
-function loadSavedNotes(val) {
-    let html = 
-    `<div class = "savednotesarea">
-        <div class="thenotes"> 
-            <div class="note">${val}</div>
-        </div>
-        <div class = "div-remove-note">
-            <button class="remove-note">X</button>
-        </div>
-    </div>`;
-
-    divElement.insertAdjacentHTML('afterend', html);
-    
-    document.querySelector(".remove-note")
-    .addEventListener("click", function(){
-        console.log(this.parentNode);
-        let removeelement = this.parentNode.parentNode;
-        containerDiv.removeChild(removeelement)
-        
-    })
-}
-
-for (let i = 0; i < savednoteslist.length; i++) {
-    loadSavedNotes(savednoteslist[i]);
-}
-
-function displaySavedNotes() {
-    const textarea = document.getElementById("textBox");
-
-    let html = 
-    `<div class = "savednotesarea">
-        <div class="thenotes"> 
-            <div class="note">${textarea.value}</div>
-        </div>
-        <div class = "div-remove-note">
-            <button class="remove-note">X</button>
-        </div>
-    </div>`;
-    if ((textarea.value).trim() !="")
-    divElement.insertAdjacentHTML('afterend', html);
-
-    savednoteslist.push(textarea.value);
-
-    localStorage.setItem("notes",JSON.stringify(savednoteslist));
-    
-document.querySelector(".remove-note").addEventListener("click", function(){
-    let removeelement = this.parentNode.parentNode;
-    containerDiv.removeChild(removeelement)
-})
-}
-
-
-
-
-//clear button function
+        textarea.value = ""; // Clear the textarea
+    }
+});
 
 clearbutton.onclick = function() {
-    const textarea = document.getElementById("textBox");
-    console.log("you cleared :", textarea.value);
-    document.getElementById("textBox").value = " ";
-}
+    textarea.value = ""; // Clear the textarea
+};
 
+function createNoteElement(noteText) {
+    const html = `
+        <div class="savednotesarea">
+            <div class="thenotes"> 
+                <div class="note">${noteText}</div>
+            </div>
+            <div class="div-remove-note">
+                <button class="remove-note">X</button>
+            </div>
+        </div>`;
+    const noteElement = document.createElement('div');
+    noteElement.classList.add("addnewnote");
+    noteElement.innerHTML = html;
+    containerDiv.appendChild(noteElement);
+
+    noteElement.querySelector(".remove-note").addEventListener("click", function() {
+        containerDiv.removeChild(noteElement);
+
+        // Update localStorage after removing the note
+        const savedNotes = JSON.parse(localStorage.getItem("notes")) || [];
+        const updatedNotes = savedNotes.filter(savedNote => savedNote !== noteText);
+        localStorage.setItem("notes", JSON.stringify(updatedNotes));
+    });
+}
